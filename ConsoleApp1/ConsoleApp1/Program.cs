bd9aaab0045aea0ed3dc9d601237eb41f07c682a
awolferret <104518827+awolferret@users.noreply.github.com>
2022-05-09 20:37:30 +0300

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Cъешь ещё этих мягких французских булок да выпей чаю";
+            string[] words = text.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+        }
+    }
+}
